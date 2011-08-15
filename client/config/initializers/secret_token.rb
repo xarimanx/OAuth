8d05c24081c35dbee6eb616fe6a9ba98587e927c
xarimanx <xarimanx@gmail.com>
2011-08-15 18:26:19 +0300

    Initial commit

---
+++ client/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Client::Application.config.secret_token = 'bbce60fa3a8e3a23d68b2855143d2279f5e968e0a0d472e34c2d20159bbbd9d2534a52c599dabacb8910f307aa63aad6f8468e0b7e14d336ec8fe0fe87c834b4'
---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Server::Application.config.secret_token = '98bea641f14d84e1a8fafad8ac8b029bd1dc89de8ad35130f00aa838b336131c7110e8c33014cda309aa467b57e88cbba82f05ef8873276fcd473a83ca42c186'
