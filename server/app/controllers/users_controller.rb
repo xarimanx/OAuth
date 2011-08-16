9adab1316c75cfc052ec471853a816720dc5f803
Ariman <xarimanx@gmail.com>
2011-08-16 23:36:07 +0300

    end with user

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -10,11 +14,37 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      flash[:success] = "Registration successful!"
+      sign_in @user
+      flash[:success] = "Registration Successful!"
       redirect_to @user
     else
       render 'new'
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
